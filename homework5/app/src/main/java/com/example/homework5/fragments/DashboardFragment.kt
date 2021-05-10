72bb993aca3e4fc672865b82f835c23d6324f216
MariamTsikhitatrishvili <mariam.tsikhitatrishvili.1@btu.edu.ge>
2021-05-10 11:32:26 +0400

    Finish Homework5

---
+++ homework5/app/src/main/java/com/example/homework5/fragments/DashboardFragment.kt
@@ -0,0 +1,7 @@
+package com.example.homework5.fragments
+import androidx.fragment.app.Fragment
+import com.example.homework5.R
+
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+}
---
+++ homework5/app/src/main/java/com/example/homework5/fragments/NotificationsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.homework5.fragments
+import androidx.fragment.app.Fragment
+import com.example.homework5.R
+
+class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
+}
